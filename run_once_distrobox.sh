f58a0cb82371cec77bff217226108221824c3a75
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-28 07:52:36 -0600

    Remove run_once*, make ansible playbook run on host.

--- run_once_distrobox.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build localhost/fedora container.
-podman build -f ~/.bootstrap/Containerfile.fedora -t "localhost/fedora"
-
-# Setup Fedora distrobox
-SHELL=/usr/bin/zsh distrobox create --image localhost/fedora --name Fedora
-distrobox upgrade Fedora # force init container
