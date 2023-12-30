5f6fb840de3f5ead51133d68b39c991cae802864
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-29 19:51:14 -0600

    Move python3-psutils install into ansible section.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -3,10 +3,13 @@
 # Install distrobox if not already.
 if ! command -v distrobox &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live distrobox
-	rpm-ostree install --assumeyes --aply-live python3-psutil
 fi
 
 # Install ansible if not already.
 if ! command -v ansible &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live ansible
+
+	# ansible's dconf module requires python3-psutil
+	# @see https://docs.ansible.com/ansible/latest/collections/community/general/dconf_module.html
+	rpm-ostree install --assumeyes --aply-live python3-psutil
 fi
