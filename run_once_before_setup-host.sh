8d7376b573fb830eba0c60321b61affe2189b027
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-30 17:06:30 -0600

    Use gnome-extensions command to install AppIndicator support.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -15,10 +15,13 @@ if ! command -v ansible &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live ansible
 fi
 
-# Setup Gnome Extension: AppIndicator and KStatusNotifierItem
+# Setup Gnome Extension: AppIndicator Support
 # @see https://extensions.gnome.org/extension/615/appindicator-support/
 #
-if ! $(gnome-extensions info appindicatorsupport@rgcjonas.gmail.com); then
-	rpm-ostree install --assumeyes --apply-live gnome-shell-extension-appindicator
+if ! gnome-extensions info appindicatorsupport@rgcjonas.gmail.com; then
+	store_url="https://extensions.gnome.org/extension-data"
+	zip="appindicatorsupportrgcjonas.gmail.com.v57.shell-extension.zip"
+	wget "$store_url/$zip"
+	gnome-extensions install $zip --force
 	gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
 fi
