23f1fcb73b04f5c951a75c8e78f6617868af2ac1
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-30 12:31:38 -0600

    Ansible no longer needs to configure dconf.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -13,8 +13,4 @@ fi
 # Install ansible if not already.
 if ! command -v ansible &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live ansible
-
-	# ansible's dconf module requires python3-psutil
-	# @see https://docs.ansible.com/ansible/latest/collections/community/general/dconf_module.html
-	rpm-ostree install --assumeyes --aply-live python3-psutil
 fi
