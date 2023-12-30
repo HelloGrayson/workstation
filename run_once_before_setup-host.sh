8d55e96c13b10a32a8d5bde86381dbb311a871e0
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-29 20:05:29 -0600

    Layer fastfetch into Silverblue host.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install fastfetch if not already
+if ! command -v fastfetch &>/dev/null; then
+	rpm-ostree install --assumeyes --apply-live fastfetch
+fi
+
 # Install distrobox if not already.
 if ! command -v distrobox &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live distrobox
