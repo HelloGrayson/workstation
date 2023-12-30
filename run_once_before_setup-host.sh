1f72ee36181ae2b677bb62b638f48cc3ed275917
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-29 19:42:26 -0600

    Set Gnome to darkmode with preferred wallpaper.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -3,6 +3,7 @@
 # Install distrobox if not already.
 if ! command -v distrobox &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live distrobox
+	rpm-ostree install --assumeyes --aply-live python3-psutil
 fi
 
 # Install ansible if not already.
