486189d05f0ff2812bc033548a2b7eef0e51cf5f
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-30 20:10:35 -0600

    Move dconf to end, since it will distort the terminal font on first run.

--- run_once_after_gnome-extensions.sh
+++ run_once_after_gnome-extensions.sh
@@ -8,6 +8,11 @@ if ! gnome-extensions info appindicatorsupport@rgcjonas.gmail.com; then
 	zip="appindicatorsupportrgcjonas.gmail.com.v57.shell-extension.zip"
 	wget "$store_url/$zip"
 	gnome-extensions install $zip --force
-fi
 
-reboot
+	# Because Gnome+Wayland does not allow extensions to be installed
+	# within the current session; enable does not work. The workaround
+	# is to reboot the system, which luckily, enables the extensions
+	# that were requested to be installed.
+	# gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+	echo "AppIndicator install requested; reboot to enable."
+fi
