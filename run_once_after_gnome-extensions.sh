5dddc6911bfa4dc12cbe870c2d4d180d2d5d867f
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-30 20:27:39 -0600

    Install Caffeine Gnome Extension.

--- run_once_after_gnome-extensions.sh
+++ run_once_after_gnome-extensions.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+store_url="https://extensions.gnome.org/extension-data"
+cd ~/Downloads
+
 # Setup Gnome Extension: AppIndicator Support
 # @see https://extensions.gnome.org/extension/615/appindicator-support/
 #
 if ! gnome-extensions info appindicatorsupport@rgcjonas.gmail.com; then
-	store_url="https://extensions.gnome.org/extension-data"
 	zip="appindicatorsupportrgcjonas.gmail.com.v57.shell-extension.zip"
 	wget "$store_url/$zip"
 	gnome-extensions install $zip --force
+fi
 
-	# Because Gnome+Wayland does not allow extensions to be installed
-	# within the current session; enable does not work. The workaround
-	# is to reboot the system, which luckily, enables the extensions
-	# that were requested to be installed.
-	# gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
-	echo "AppIndicator install requested; reboot to enable."
+# Setup Gnome Extension: Caffeine
+# @see https://extensions.gnome.org/extension/517/caffeine/
+#
+if ! gnome-extensions info caffeine@patapon.info; then
+	store_url="https://extensions.gnome.org/extension-data"
+	zip="caffeinepatapon.info.v52.shell-extension.zip"
+	wget "$store_url/$zip"
+	gnome-extensions install $zip --force
 fi
