ec912a21354cfc241490e60d7958c58727d7c364
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-31 11:59:20 -0600

    Install Mullvad using their Yum repository.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -14,3 +14,17 @@ fi
 if ! command -v ansible &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live ansible
 fi
+
+# Install mullvad if not already.
+#
+# @see https://docs.fedoraproject.org/en-US/fedora-silverblue/troubleshooting/#_adding_external_package_repositories
+# @see https://mullvad.net/en/help/install-mullvad-app-linux#fedora
+#
+if ! command -v mullvad &>/dev/null; then
+	cd ~/Downloads/
+	wget https://repository.mullvad.net/rpm/stable/mullvad.repo
+	sudo install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
+	wget https://repository.mullvad.net/rpm/mullvad-keyring.asc
+	sudo install -o 0 -g 0 -m644 mullvad-keyring.asc /etc/pki/rpm-gpg/mullvad-keyring.asc
+	rpm-ostree install --assumeyes --apply-live mullvad-vpn
+fi
