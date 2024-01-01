37dc555d265f2be5f3a580757a0c0d5a12751266
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-31 18:46:55 -0600

    Consolidate host setup that requires reboot.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -19,32 +19,3 @@ fi
 if ! command -v ansible &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live ansible
 fi
-
-# Install mullvad if not already.
-#
-# @see https://docs.fedoraproject.org/en-US/fedora-silverblue/troubleshooting/#_adding_external_package_repositories
-# @see https://mullvad.net/en/help/install-mullvad-app-linux#fedora
-#
-if ! command -v mullvad &>/dev/null; then
-	cd ~/Downloads/
-	wget https://repository.mullvad.net/rpm/stable/mullvad.repo
-	sudo install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
-	wget https://repository.mullvad.net/rpm/mullvad-keyring.asc
-	sudo install -o 0 -g 0 -m644 mullvad-keyring.asc /etc/pki/rpm-gpg/mullvad-keyring.asc
-	rpm-ostree install --assumeyes --apply-live mullvad-vpn
-	sudo systemctl enable mullvad-daemon # app available after reboot
-fi
-
-# Install opensnitch if not already
-#
-# @see https://github.com/evilsocket/opensnitch/wiki/Installation
-# @see https://github.com/evilsocket/opensnitch/releases
-# @see https://github.com/coreos/rpm-ostree/issues/1978
-#
-if ! command -v opensnitchd &>/dev/null; then
-	cd ~/Downloads/
-	wget https://github.com/evilsocket/opensnitch/releases/download/v1.6.2/opensnitch-1.6.2-1.x86_64.rpm
-	wget https://github.com/evilsocket/opensnitch/releases/download/v1.6.4/opensnitch-ui-1.6.4-1.noarch.rpm
-	rpm-ostree install --assumeyes --apply-live opensnitch-*.rpm
-	sudo systemctl enable opensnitch # app available after reboot
-fi
