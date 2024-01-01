e5d81a853eed59c3e3ac753ec50248f1c22e5592
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2023-12-31 18:07:28 -0600

    Reorder layered packages.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -5,6 +5,11 @@ if ! command -v fastfetch &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live fastfetch
 fi
 
+# Install age if not already.
+if ! command -v age &>/dev/null; then
+	rpm-ostree install --assumeyes --apply-live age
+fi
+
 # Install distrobox if not already.
 if ! command -v distrobox &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live distrobox
@@ -13,11 +18,6 @@ fi
 # Install ansible if not already.
 if ! command -v ansible &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live ansible
-fi
-
-# Install age if not already.
-if ! command -v age &>/dev/null; then
-	rpm-ostree install --assumeyes --apply-live age
 fi
 
 # Install mullvad if not already.
