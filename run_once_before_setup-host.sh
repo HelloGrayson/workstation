302264f7994f5a3b21484a6f3a656e96d0f79066
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2024-01-02 12:35:15 -0600

    Layer restic into host.

--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Install fastfetch if not already
+# Install fastfetch if not already.
 if ! command -v fastfetch &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live fastfetch
-fi
-
-# Install age if not already.
-if ! command -v age &>/dev/null; then
-	rpm-ostree install --assumeyes --apply-live age
 fi
 
 # Install distrobox if not already.
 if ! command -v distrobox &>/dev/null; then
 	rpm-ostree install --assumeyes --apply-live distrobox
+fi
+
+# Install restic if not already.
+if ! command -v restic &>/dev/null; then
+	rpm-ostree install --assumeyes --apply-live restic
 fi
 
 # Install ansible if not already.
