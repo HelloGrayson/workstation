e3d0be6fa9db03a1985de169ccf4f48669103376
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2024-01-03 20:06:36 -0600

    Enforce logged into Bitwarden.

---
+++ run_before_bitwarden.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install bw if not already.
+if ! command -v bw &>/dev/null; then
+	cd ~/Downloads/
+	wget --content-disposition "https://vault.bitwarden.com/download/?app=cli&platform=linux"
+	unzip -u bw-linux-*.zip
+	mv bw ~/bin/
+	chmod +x ~/bin/bw
+fi
+
+# Login if not already.
+if ! bw login --check; then
+	bw login
+fi
--- run_once_before_setup-host.sh
+++ run_once_before_setup-host.sh
@@ -4,12 +4,3 @@
 rpm-ostree install \
 	--assumeyes --apply-live --idempotent \
 	fastfetch distrobox restic
-
-# Install bw if not already.
-if ! command -v bw &>/dev/null; then
-	cd ~/Downloads/
-	wget --content-disposition "https://vault.bitwarden.com/download/?app=cli&platform=linux"
-	unzip -u bw-linux-*.zip
-	mv bw ~/bin/
-	chmod +x ~/bin/bw
-fi
