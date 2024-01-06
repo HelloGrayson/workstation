f27b17678f9ad39673849281bd04e216b0b3c102
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2024-01-06 16:35:14 -0600

    Test Gnome Keyring restore.

--- run_once_setup-keys.sh
+++ run_once_setup-keys.sh
@@ -4,12 +4,12 @@ set -x
 # Setup SSH (Fedora)
 # @see https://unix.stackexchange.com/a/581000
 # exec ssh-agent bash
-ssh-add ~/.ssh/id_ed25519
+#ssh-add ~/.ssh/id_ed25519
 ls -al ~/.ssh
 
 # Setup GPG
 # @see https://dev.to/mattdark/backup-and-restore-github-gpg-and-ssh-keys-2a12
-gpg --import ~/.gnupg/HelloGrayson.asc
+#gpg --import ~/.gnupg/HelloGrayson.asc
 gpg --list-secret-keys --keyid-format LONG
 
 # Enable commit access to local Chezmoi repo
