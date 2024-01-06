ee4aa1d861c6f9399e41ffaf887e6efe19d5abfd
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2024-01-06 15:31:26 -0600

    Add GPG key.

--- run_once_setup-keys.sh
+++ run_once_setup-keys.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -x
 
-# Setup SSH
-exec ssh-agent bash
+# Setup SSH (Fedora)
+# @see https://unix.stackexchange.com/a/581000
+# exec ssh-agent bash
 ssh-add ~/.ssh/id_ed25519
 
 # Setup GPG
+# @see https://dev.to/mattdark/backup-and-restore-github-gpg-and-ssh-keys-2a12
+gpg --import ~/.gnupg/HelloGrayson.asc
