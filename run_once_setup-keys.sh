90780aafe9214521d3c140734f1f244a8b58fcdc
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2024-01-06 17:37:34 -0600

    Add comments about Gnome Keyring to key setup.

--- run_once_setup-keys.sh
+++ run_once_setup-keys.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -x
 
+# So long as the user password manages the ~/.local/share/keyrings password,
+# Gnome will automatically unlock SSH and GPG keys.
+#
+# @see https://wiki.gnome.org/Projects/GnomeKeyring/
+
 # Setup SSH (Fedora)
 # @see https://unix.stackexchange.com/a/581000
 # exec ssh-agent bash
-#ssh-add ~/.ssh/id_ed25519
+#ssh-add ~/.ssh/id_ed25519 # Gnome Keyring does this.
 ls -al ~/.ssh
 
 # Setup GPG
 # @see https://dev.to/mattdark/backup-and-restore-github-gpg-and-ssh-keys-2a12
-#gpg --import ~/.gnupg/HelloGrayson.asc
+#gpg --import ~/.gnupg/HelloGrayson.asc # Gnome Keyring does this.
 gpg --list-secret-keys --keyid-format LONG
 
 # Enable commit access to local Chezmoi repo
