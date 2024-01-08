b214d4c6f0701e1547719c2233ad3d9007aa5c86
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2024-01-08 12:18:35 -0600

    Fedora Media Writer replaces Balena Etcher.

--- run_once_setup-appimages.sh
+++ run_once_setup-appimages.sh
@@ -17,15 +17,6 @@ main() {
 		chmod +x gearlever_system76keyboardconfigurator_161104.appimage
 	fi
 
-	# Balena Etcher - Flash OS images to SD cards & USB drives, safely and easily.
-	# @see https://etcher.balena.io/#download-etcher
-	#
-	if ! test -f gearlever_balenaetcher_e89f5c.appimage; then
-		wget https://github.com/balena-io/etcher/releases/download/v1.18.11/balenaEtcher-1.18.11-x64.AppImage
-		mv balenaEtcher-1.18.11-x64.AppImage gearlever_balenaetcher_e89f5c.appimage
-		chmod +x gearlever_balenaetcher_e89f5c.appimage
-	fi
-
 	# Ente - Store, share, and rediscover your memories with absolute privacy
 	# @see https://ente.io/download
 	#
