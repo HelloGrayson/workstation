c156e8029da3a23fa827a8df659290460ab1a8fc
@HelloGrayson <263587+HelloGrayson@users.noreply.github.com>
2024-01-11 17:06:03 -0600

    Allow chezmoi and bw to be upgraded.

--- upgrade.sh
+++ upgrade.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/bash
 #
 # Perform a full update of Silverblue workstation.
+#
+# Manually managed: opensnitch & gnome appindicator plugin.
 
 set -xeuo pipefail
 
 main() {
-	# TODO: update bw, chezmoi
+	bw-upgrade
+	chezmoi upgrade
 	flatpak update --user --assumeyes
 	distrobox upgrade --all
 	rpm-ostree upgrade
+}
+
+bw-upgrade() {
+	cd ~/Downloads/ || exit
+
+	# get latest bw binary
+	rm -rf bw-linux-*.zip
+	wget --content-disposition "https://vault.bitwarden.com/download/?app=cli&platform=linux"
+	unzip -u bw-linux-*.zip
+
+	# replace bw
+	rm -f ~/.local/bin/bw
+	mv bw ~/.local/bin/
+	chmod +x ~/.local/bin/bw
 }
 
 main "${@}"
